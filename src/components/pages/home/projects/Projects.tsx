64ba8791d030ef7a535ad121c681c02f6e65601b
Vinicius Carvalho dos Santos <vinicius.santos@ibratan.com.br>
2022-07-08 21:18:55 -0300

    Change projects structure

--- src/components/pages/home/projects/Projects.tsx
+++ src/components/pages/home/projects/Projects.tsx
@@ -4,7 +4,7 @@ import './Projects.css';
 import '../../../../i18n/config';
 import {useTranslation} from "react-i18next";
 import projects from "../../../../assets/projects/projects.json"
-import {Grid} from "@mui/material";
+import {Chip, Divider, Grid} from "@mui/material";
 import TokenCanvas from "./canvas/TokenCanvas";
 
 function Projects() {
@@ -19,48 +19,59 @@ function Projects() {
             <p className={'project-description'}>{t("projects.description")}</p>
 
             <TokenCanvas/>
+            {projects.map((project) => (
 
-            <Grid sx={{flexGrow: 1}} container spacing={4}>
-                <Grid item xs={12}>
-                    <Grid container justifyContent="center" spacing={4}>
-                        {projects.map((project) => (
-                            <Grid key={project.name} item>
-                                <Card id={"project-card"}>
-                                    <Card.Img variant="top" className={"project-image"} src={project.image}/>
-                                    <Card.Body id={'card-body'}>
-                                        <div>
-                                            <Card.Title>
+                <Grid sx={{flexGrow: 1}} container spacing={4}>
+                    <Grid item xs={12}>
+                        <Divider orientation="horizontal" flexItem>
+                            <Chip label={project.title} />
+                        </Divider>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Grid container justifyContent="center" spacing={4}>
+                            {project.projects.map((project) => (
+                                <Grid key={project.name} item>
+                                    <Card id={"project-card"}>
+                                        <Card.Img variant="top" className={"project-image"} src={project.image}/>
+                                        <Card.Body id={'card-body'}>
+                                            <div>
+                                                <Card.Title>
                                         <span
                                             className={'shadows-into-light-font'}>{t(`projects.${project.name}.title`)} </span>
-                                                <Badge bg="secondary">{t(`projects.${project.name}.tech`)}</Badge>
-                                            </Card.Title>
+                                                    <Badge bg="secondary">{t(`projects.${project.name}.tech`)}</Badge>
+                                                </Card.Title>
 
-                                            <Card.Text id={'card-content'}>
-                                                {t(`projects.${project.name}.content`)}
-                                            </Card.Text>
-                                        </div>
+                                                <Card.Text id={'card-content'}>
+                                                    {t(`projects.${project.name}.content`)}
+                                                </Card.Text>
+                                            </div>
 
-                                        <div>
-                                            <Card.Text>
-                                                {t(`projects.${project.name}.moreinfo`)}
-                                            </Card.Text>
+                                            <div>
+                                                <Card.Text>
+                                                    {t(`projects.${project.name}.moreinfo`)}
+                                                </Card.Text>
 
-                                            <Card.Link href={project.github} target={'_blank'}>
-                                                <Button variant="outline-info">{t(`projects.${project.name}.github`)}
-                                                </Button></Card.Link>
+                                                <Card.Link href={project.github} target={'_blank'}>
+                                                    <Button
+                                                        variant="outline-info">{t(`projects.${project.name}.github`)}
+                                                    </Button></Card.Link>
 
-                                            <Card.Link href={project.documentation} target={'_blank'}>
-                                                <Button variant="outline-info">{t(`projects.${project.name}.documentation`)}
-                                                </Button></Card.Link>
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            </Grid>
-                        ))}
+                                                <Card.Link href={project.documentation} target={'_blank'}>
+                                                    <Button
+                                                        variant="outline-info">{t(`projects.${project.name}.documentation`)}
+                                                    </Button></Card.Link>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Grid>
+                            ))}
 
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
+            ))}
+
         </Container>
     )
 
